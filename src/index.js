b7b0442ba6c9e36ec50b393cf26fe7d490ca9b2d
KrishRaj0 <109430557+KrishRaj1@users.noreply.github.com>
2025-09-19 00:03:57 +0530

    Adding some new get and post route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+let notes = []
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send("This is a notes App")   
+})
+app.get('/notes', (req, res) =>{
+    res.json(notes)
+})
+app.post('/notes', (req, res) => {
+    const note = {id : Date.now(), text : req.body.text}
+    notes.push(note)
+    res.status(201).json(note)
 })
 app.listen(PORT, () =>{
     console.log(`Application is runing on ${PORT}`)
