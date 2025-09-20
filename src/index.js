23c6b9de02e79fccc9e99de2bb56387ba8521fdc
KrishRaj0 <109430557+KrishRaj1@users.noreply.github.com>
2025-09-20 14:55:03 +0530

    Add delete and put route for notes update

--- src/index.js
+++ src/index.js
@@ -15,6 +15,27 @@ app.post('/notes', (req, res) => {
     notes.push(note)
     res.status(201).json(note)
 })
+app.put('/notes/:id',(req, res) => {
+    const id = parseInt(req.params.id)
+    const index = notes.findIndex(n => n.id === id)
+
+    if(index === -1){
+       return res.status(404).send("Note not found")
+    }
+    notes[index].text = req.body.text
+    res.json(notes[index])
+})
+
+app.delete('/notes/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const noteExits = notes.some(n => n.id === id)
+    if(!noteExits){
+        return res.status(404).send("Note not found")
+    }
+    notes= notes.filter(n => n.id !== id)
+    res.status(200).send("Note deleted successfully")
+})
+
 app.listen(PORT, () =>{
     console.log(`Application is runing on ${PORT}`)
 })
